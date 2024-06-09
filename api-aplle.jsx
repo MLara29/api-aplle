5652f9c229c44763c25bb759bf889f24ebb0b468
Miqueias Lara <miqueias.lara@gmail.com>
2024-06-09 20:09:39 -0300

    teste

---
+++ api-aplle.jsx
@@ -0,0 +1,201 @@
+export const SHOP_DATA = [
+    {
+      title: 'Mac',
+      items: [
+        {
+          id: 1,
+          name: 'MacBook Pro 14" M3/16GB/512GB SSD',
+          imageUrl: 'https://www.goimports.com.br/image/catalog/0%20macm3/M.3/mbp14-spacegray-gallery1-202310.png',
+          price: 11999.00,
+        },
+        {
+          id: 2,
+          name: 'MacBook Air 13" M1/8GB/256Gb SSD',
+          imageUrl: 'https://www.goimports.com.br/image/catalog/Macs/macbook-air-silver-config-201810.jpg',
+          price: 5299.00,
+        },
+        {
+          id: 3,
+          name: 'Mac Mini M2 Pro, 16GB de Ram, 512GB SSD (2023)',
+          imageUrl: 'https://www.goimports.com.br/image/catalog/0macpro14/mini/mac-mini-202301-gallery-1.jpeg',
+          price: 8899.00,
+        },
+        {
+          id: 4,
+          name: 'Mac Studio M2 Max 32GB de Ram 1TB SSD',
+          imageUrl: 'https://www.goimports.com.br/image/catalog/Studio/mac-studio-202203-gallery-1.png',
+          price: 18199.00,
+        },
+        {
+          id: 5,
+          name: 'MacBook Air 13" (2024) M3 / 16GB / 256GB SSD',
+          imageUrl: 'https://www.goimports.com.br/image/catalog/Mac%20m2/macbook-air-midnight-gallery2-20220606_GEO_US.png',
+          price: 19999.00,
+        },
+        {
+          id: 6,
+          name: 'MacBook Air 15" (2024) M3 / 16GB / 256GB SSD',
+          imageUrl: 'https://www.goimports.com.br/image/catalog/0macpro14/002/mba15-midnight-select-202306.jpeg',
+          price: 11899.00,
+        },
+        {
+          id: 7,
+          name: 'Mac Pro M2 Ultra 64GB de Ram 1TB SSD',
+          imageUrl: 'https://www.goimports.com.br/image/catalog/00macpro/mac-pro-tower-2023-gallery1.jpeg',
+          price: 159999.00,
+        },
+      ],
+    },
+    {
+      title: 'iPhones',
+      items: [
+        {
+          id: 8,
+          name: 'iPhone 13 128GB',
+          imageUrl: 'https://www.goimports.com.br/image/catalog/13%20a/azul2.PNG',
+          price: 2799.00,
+        },
+        {
+          id: 9,
+          name: 'iPhone 14 128GB',
+          imageUrl: 'https://www.goimports.com.br/image/catalog/00iphone15/iphone-15-finish-select-202309-6-1inch-pink.png',
+          price: 3799.00,
+        },
+        {
+          id: 10,
+          name: 'iPhone 15 128GB',
+          imageUrl: 'https://www.goimports.com.br/image/catalog/00iphone15/iphone-15-finish-select-202309-6-1inch-black.png',
+          price: 110,
+        },
+        {
+          id: 11,
+          name: 'iPhone 15 Plus 128GB',
+          imageUrl: 'https://www.goimports.com.br/image/catalog/00iphone15/iphone-15-finish-select-202309-6-1inch_AV1.png',
+          price: 5499.00,
+        },
+        {
+          id: 12,
+          name: 'iPhone 15 Pro 128GB',
+          imageUrl: 'https://www.goimports.com.br/image/catalog/00iphone15/iphone-15-pro-finish-select-202309-6-1inch_AV1.png',
+          price: 5599.00 ,
+        },
+        {
+          id: 13,
+          name: 'iPhone 15 Pro Max 1TB',
+          imageUrl: 'https://www.goimports.com.br/image/catalog/00iphone15/iphone-15-pro-finish-select-202309-6-7inch-bluetitanium.png',
+          price: 9399.00,
+        },
+      ],
+    },
+    {
+      title: 'iPad',
+      items: [
+        {
+          id: 14,
+          name: 'iPad 10˚ Geração 64GB Prateado Wi-Fi + Celular',
+          imageUrl: 'https://www.goimports.com.br/image/catalog/0%20ipad%2010/ipad/ipad-2022-hero-silver-wifi-select.png',
+          price: 4199.00,
+        },
+        {
+          id: 15,
+          name: 'iPad Air 5ª Geração 256GB Wi-Fi',
+          imageUrl: 'https://www.goimports.com.br/image/catalog/Air%205/ipad-air-select-wifi-blue-202203.png',
+          price: 4399.00,
+        },
+        {
+          id: 16,
+          name: 'iPad Air M2 11" 128GB Wi-Fi',
+          imageUrl: 'https://www.goimports.com.br/image/catalog/0%20novos%20ipa/ipad-air-finish-select-gallery-202405-13inch-space-gray-wificell.png',
+          price: 5099.00,
+        },
+        {
+          id: 17,
+          name: 'iPad Air M2 13" 1TB Wi-Fi + Celular',
+          imageUrl: 'https://www.goimports.com.br/image/catalog/0%20novos%20ipa/ipad-air-finish-select-gallery-202405-13inch-space-gray-wificell.png',
+          price: 11499.00,
+        },
+        {
+          id: 18,
+          name: 'iPad Air M2 11" 512GB Wi-Fi + Celular',
+          imageUrl: 'https://www.goimports.com.br/image/catalog/0%20novos%20ipa/ipad-air-finish-select-gallery-202405-13inch-space-gray-wificell.png',
+          price: 8399.00,
+        },
+      ],
+    },
+    {
+      title: 'Watch',
+      items: [
+        {
+          id: 19,
+          name: 'Watch SE 40mm GPS 2° Geração',
+          imageUrl: 'https://www.goimports.com.br/image/catalog/0%20SE%202/MKU83_VW_34FR+watch-40-alum-midnight-nc-se_VW_34FR_WF_CO_GEO_BR.jpeg',
+          price: 1499.00,
+        },
+        {
+          id: 20,
+          name: 'Watch SE 44mm GPS 2° Geração - Cellular',
+          imageUrl: 'https://www.goimports.com.br/image/catalog/0%20SE%202/002/MKUU3_VW_34FR+watch-44-alum-starlight-cell-se_VW_34FR_WF_CO_GEO_BR.jpeg',
+          price: 1899.00,
+        },
+        {
+          id: 21,
+          name: 'Watch Series 9 41mm Caixa de aço Pulseira milanês GPS+Celular',
+          imageUrl: 'https://www.goimports.com.br/image/catalog/00Watc9/ML773_VW_34FR+watch-case-45-stainless-graphite-s9_VW_34FR+watch-face-45-stainless-graphite-s9_VW_34FR.png',
+          price: 4899.00,
+        },
+        {
+          id: 22,
+          name: 'Watch Ultra 2 49mm Pulseira loop Alpina Média',
+          imageUrl: 'https://www.goimports.com.br/image/catalog/00Watc9/MT5N3ref_VW_34FR+watch-49-titanium-ultra2_VW_34FR+watch-face-49-alpine-ultra2_VW_34FR_GEO_BR.png',
+          price: 4799.00,
+        },
+        {
+          id: 23,
+          name: 'Watch Ultra 2 49mm Pulseira loop Trail M/G',
+          imageUrl: 'hhttps://www.goimports.com.br/image/catalog/00Watc9/MT5W3ref_VW_34FR+watch-49-titanium-ultra2_VW_34FR+watch-face-49-trail-ultra2_VW_34FR_GEO_BR.png',
+          price: 4799.00,
+        },
+        {
+          id: 24,
+          name: 'Watch Ultra 2 49mm Pulseira loop Trail P/M',
+          imageUrl: 'https://www.goimports.com.br/image/catalog/00Watc9/MT5Y3ref_VW_34FR+watch-49-titanium-ultra2_VW_34FR+watch-face-49-trail-ultra2_VW_34FR_GEO_BR.png',
+          price: 4799.00 ,
+        },
+        {
+          id: 25,
+          name: 'Watch Ultra 2 49mm Pulseira Oceano',
+          imageUrl: 'https://www.goimports.com.br/image/catalog/00Watc9/MT653_VW_34FR+watch-49-titanium-ultra2_VW_34FR+watch-face-49-ocean-ultra2_VW_34FR_GEO_BR.png',
+          price: 4799.00,
+        },
+      ],
+    },
+    {
+      title: 'AirPods',
+      items: [
+        {
+          id: 26,
+          name: 'Airpods Max',
+          imageUrl: 'https://www.goimports.com.br/image/catalog/0ssd%20sansung/airpods-max-select-skyblue-202011.png',
+          price: 3699.00,
+        },
+        {
+          id: 27,
+          name: 'Apple AirPods 2ª Geração',
+          imageUrl: 'https://www.goimports.com.br/image/catalog/Acessorios/MV7N2.png',
+          price: 849.00,
+        },
+        {
+          id: 28,
+          name: 'Apple AirPods Pro 2° Geração - USB-C',
+          imageUrl: 'https://www.goimports.com.br/image/catalog/0%20Air%20pro%202/MQD83.jpeg',
+          price: 1799.00,
+        },
+        {
+          id: 29,
+          name: 'Beats Fit Pro',
+          imageUrl: 'https://www.goimports.com.br/image/catalog/0%20Air%20pro%202/Beat/MK2F3_AV5.jpg',
+          price: 1249.00,
+        },
+      ],
+    },
+  ];
